48021a397ec145bc0f78c39c358c128a8fc1526f
CodingRub <rubstr@protonmail.com>
2023-09-28 19:20:37 +0200

    feat: add Ingredient entity

---
+++ src/Entity/Ingredient.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\IngredientRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: IngredientRepository::class)]
+#[ApiResource]
+class Ingredient
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Aliment $aliment = null;
+
+    #[ORM\Column]
+    private ?int $quantite = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAliment(): ?Aliment
+    {
+        return $this->aliment;
+    }
+
+    public function setAliment(?Aliment $aliment): static
+    {
+        $this->aliment = $aliment;
+
+        return $this;
+    }
+
+    public function getQuantite(): ?int
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(int $quantite): static
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+}
