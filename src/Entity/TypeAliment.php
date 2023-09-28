fbd71b2d5239a57994c326814943ad4dde8a4dcd
CodingRub <rubstr@protonmail.com>
2023-09-28 19:17:25 +0200

    feat: add Aliment entity

---
+++ src/Entity/TypeAliment.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\TypeAlimentRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TypeAlimentRepository::class)]
+#[ApiResource]
+class TypeAliment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\OneToMany(mappedBy: 'type', targetEntity: Aliment::class, orphanRemoval: true)]
+    private Collection $aliments;
+
+    public function __construct()
+    {
+        $this->aliments = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Aliment>
+     */
+    public function getAliments(): Collection
+    {
+        return $this->aliments;
+    }
+
+    public function addAliment(Aliment $aliment): static
+    {
+        if (!$this->aliments->contains($aliment)) {
+            $this->aliments->add($aliment);
+            $aliment->setType($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAliment(Aliment $aliment): static
+    {
+        if ($this->aliments->removeElement($aliment)) {
+            // set the owning side to null (unless already changed)
+            if ($aliment->getType() === $this) {
+                $aliment->setType(null);
+            }
+        }
+
+        return $this;
+    }
+}
